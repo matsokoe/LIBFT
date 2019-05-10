78aecd7a3936ada6acef1d54853ec3dc39615209
Marem Tsokoeva <matsokoe@w-r5-p2.s19.be>
2019-05-10 12:06:42 +0200

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: matsokoe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 17:10:13 by matsokoe          #+#    #+#             */
-/*   Updated: 2019/05/09 17:23:30 by matsokoe         ###   ########.fr       */
+/*   Updated: 2019/05/10 11:52:52 by matsokoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
+	size_t			i;
+	unsigned char	*buf;
 
-}
-
-int 	main()
-{
-	return (0);
+	i = 0;
+	buf = b;
+	if (len == 0)
+		return (b);
+	while (i < len)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (buf);
 }
