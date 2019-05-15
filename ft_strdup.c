284f26f93037f9d0d2f430d47a49378011312e5b
Marem Tsokoeva <matsokoe@w-r5-p2.s19.be>
2019-05-15 12:20:17 +0200

    new fonctions strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,8 +6,39 @@
 /*   By: matsokoe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 17:12:30 by matsokoe          #+#    #+#             */
-/*   Updated: 2019/05/09 17:12:32 by matsokoe         ###   ########.fr       */
+/*   Updated: 2019/05/15 12:20:03 by matsokoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+#include <stdlib.h>
 
+int		ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	char	*dst;
+	char	*buf_s1;
+
+	i = 0;
+	buf_s1 = (char *)s1;
+	dst = (char *)malloc(sizeof(char) * ft_strlen(buf_s1) + 1);
+	if (dst == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		dst[i] = buf_s1[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
