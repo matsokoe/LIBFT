62560d2b0dfb7fa6a3ef4b261c0ed52625752948
Marem Tsokoeva <matsokoe@w-r5-p2.s19.be>
2019-05-21 21:02:10 +0200

    strequ

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: matsokoe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 16:28:51 by matsokoe          #+#    #+#             */
-/*   Updated: 2019/05/21 14:09:14 by matsokoe         ###   ########.fr       */
+/*   Updated: 2019/05/21 21:01:38 by matsokoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,9 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
+	if (!s1 || !s2)
+		return (0);
+	if (strcmp(s1, s2) == 0)
+		return (1);
+	return (0);
 }
